26599d2a851d10bd08ffe028204694437fbb2d94
sumeyyegencer <sumeyyegencer@gmail.com>
2022-12-26 23:40:06 +0300

    Edited.

--- MvcKutuphane/Models/Entity/Model1.Designer.cs
+++ MvcKutuphane/Models/Entity/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Sümmeye PC\Desktop\MvcKutuphane-KitapIslemleri\MvcKutuphane-KitapIslemleri\MvcKutuphane\Models\Entity\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Sümmeye PC\Desktop\Yazılım\PROJELER\MvcKutuphane-KitapIslemleri\MvcKutuphane-KitapIslemleri\MvcKutuphane\Models\Entity\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
